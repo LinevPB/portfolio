48e6bbf807e117fa1ccee517a35ee7273c943d2c
Kamil Socha <45857349+LinevPB@users.noreply.github.com>
2024-07-22 01:59:06 +0200

    prototype

---
+++ script.js
@@ -0,0 +1,11 @@
+function showDesc()
+{
+    const box = document.getElementById("project-desc");
+    box.style.visibility = "visible";
+}
+
+function hideDesc()
+{
+    const mark = document.getElementById("project-desc");
+    mark.style.visibility = "hidden";
+}
